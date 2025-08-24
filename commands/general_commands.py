720ca6447a8600a29c8fb61174feb355323a7203
Otodeodo <alexaopineda@gmail.com>
2025-08-23 21:31:13 -0600

    h

--- commands/general_commands.py
+++ commands/general_commands.py
@@ -155,8 +155,9 @@ def setup(tree: app_commands.CommandTree, client: discord.Client):
         )
         
         # Comandos de Owner (solo visible para owners)
-        from utils import OWNER_IDS
-        if str(interaction.user.id) in OWNER_IDS:
+        from config import OWNER_ROLE_ID
+        owner_role = discord.utils.get(interaction.user.roles, id=OWNER_ROLE_ID)
+        if owner_role is not None:
             embed.add_field(
                 name="👑 Comandos de Owner",
                 value="`/add_gamecoins` - Añadir GameCoins a un usuario\n"
